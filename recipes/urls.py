f95c47e574bb8d86d8132ce519b9d6423ef38df9
Natalicio Nascimento <natalicio.eng.soft@gmail.com>
2024-03-26 23:19:25 -0300

    adicionando id nas recipes e name unico da url

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name='recipes-home'),
     path('sobre/', views.sobre),
     path('contatos/', views.contatos),
-    path('recipes/<int:id>', views.recipe),
+    path('recipes/<int:id>', views.recipe, name='recipes-recipe'),
 
 ]
