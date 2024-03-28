a06869da90e19966e50324f495d5ee2614554d0b
Natalicio Nascimento <natalicio.eng.soft@gmail.com>
2024-03-28 15:38:41 -0300

    rota para receitas por categorias e puxando os dados de forma dinamica

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from . import views
 
+app_name='recipes'
+
 urlpatterns = [
     path('', views.home, name='recipes-home'),
     path('sobre/', views.sobre),
     path('contatos/', views.contatos),
+    path('recipes/category/<int:category_id>', views.category, name='category'),
     path('recipes/<int:id>', views.recipe, name='recipes-recipe'),
+
 
 ]
