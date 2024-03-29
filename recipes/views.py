82d545ac10635e9d307b4c886c322b8359350ecb
Natalicio Nascimento <natalicio.eng.soft@gmail.com>
2024-03-28 23:23:31 -0300

    adicionando classes css e corrigindo o metodo recipe

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render, get_list_or_404
-from django.http import Http404, HttpResponse
-from utils.recipes.factory import make_recipe
 from .models import Recipe 
 
 def home(request):
@@ -21,7 +19,7 @@ def category(request, category_id):
     })
 
 def recipe(request, id):
-    recipe = Recipe.objects.filter(id= id).first()
+    recipe = Recipe.objects.filter(id= id, is_published=True).first()
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe' : recipe,
         'is_detail_page' : True, 
