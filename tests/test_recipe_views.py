3107f8815db7c6848ee60b9a810fd095cd43689a
Natalicio Nascimento <natalicio.eng.soft@gmail.com>
2024-04-05 16:10:37 -0300

    testando status_code home

--- tests/test_recipe_views.py
+++ tests/test_recipe_views.py
@@ -15,4 +15,7 @@ class RecipeViewsTest(TestCase):
     def test_recipe_find_one_recipe_by_id_view_is_correct(self):
         view = resolve(reverse('recipes:recipes-recipe', kwargs={'id': 1}))
         self.assertIs(view.func, views.recipe)
-    
+
+    def test_recipe_home_view_return_status_code_OK(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertEqual(response.status_code, 200 )
