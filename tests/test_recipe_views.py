41a7c4a5c5e03758ddb6c38176692405cadf3690
Natalicio Nascimento <natalicio.eng.soft@gmail.com>
2024-04-05 17:05:44 -0300

    test category view e recipe detail return 404  if no recipes founds

--- tests/test_recipe_views.py
+++ tests/test_recipe_views.py
@@ -8,14 +8,6 @@ class RecipeViewsTest(TestCase):
         view = resolve(reverse('recipes:home'))
         self.assertIs(view.func, views.home)
 
-    def test_recipe_category_view_is_correct(self):
-        view = resolve(reverse('recipes:category', kwargs={'category_id': 1}))
-        self.assertIs(view.func, views.category)
-
-    def test_recipe_find_one_recipe_by_id_view_is_correct(self):
-        view = resolve(reverse('recipes:recipes-recipe', kwargs={'id': 1}))
-        self.assertIs(view.func, views.recipe)
-
     def test_recipe_home_view_return_status_code_OK(self):
         response = self.client.get(reverse('recipes:home'))
         self.assertEqual(response.status_code, 200 )
@@ -23,3 +15,19 @@ class RecipeViewsTest(TestCase):
     def test_recipe_home_template_no_recipe_found_if_no_recipes(self):
         response = self.client.get(reverse('recipes:home'))
         self.assertIn( 'No recipes found',response.content.decode('utf-8'))
+
+    def test_recipe_category_view_is_correct(self):
+        view = resolve(reverse('recipes:category', kwargs={'category_id': 1}))
+        self.assertIs(view.func, views.category)
+
+    def test_recipe_category_view_return_404_if_no_recipes_founds(self):
+        response = self.client.get(reverse('recipes:category', kwargs={'category_id': 1000}))
+        self.assertEqual(response.status_code, 404)
+
+    def test_recipe_detail_view_function_is_correct(self):
+        view = resolve(reverse('recipes:recipes-recipe', kwargs={'id': 1}))
+        self.assertIs(view.func, views.recipe)
+
+    def test_recipe_detail_view_return_404_if_no_recipes_founds(self):
+        response = self.client.get(reverse('recipes:recipes-recipe', kwargs={'id': 1000}))
+        self.assertEqual(response.status_code, 404)
