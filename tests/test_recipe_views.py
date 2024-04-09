437e3a9cca59f4366d9d8af3a79add6783413d05
Natalicio Nascimento <natalicio.eng.soft@gmail.com>
2024-04-08 22:59:03 -0300

    criando metodos setupm teardown. Extrando dados para a classe RecipeTestBase

--- tests/test_recipe_views.py
+++ tests/test_recipe_views.py
@@ -1,9 +1,13 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
 from recipes import views
-from recipes.models import Category, Recipe, User
+from recipes.models import Recipe
+from .test_recipe_base import RecipeTestBase
+    
+class RecipeViewsTest(RecipeTestBase):
 
-class RecipeViewsTest(TestCase):
+    def tearDown(self) -> None:
+        return super().tearDown()
 
     def test_recipe_home_view_is_correct(self):
         view = resolve(reverse('recipes:home'))
@@ -18,33 +22,11 @@ class RecipeViewsTest(TestCase):
         self.assertTemplateUsed(response, 'recipes/pages/home.html')
 
     def test_recipe_home_template_no_recipe_found_if_no_recipes(self):
+        Recipe.objects.filter(pk=1).delete()
         response = self.client.get(reverse('recipes:home'))
         self.assertIn( 'No recipes found',response.content.decode('utf-8'))
 
     def test_recipe_home_template_loads_recipes(self):
-        category = Category.objects.create(name='category')
-        author = User.objects.create_user(
-            first_name='user',
-            last_name='name',
-            username='username',
-            password='123456',
-            email='username@email.com',
-        )
-        recipe = Recipe.objects.create(
-            category=category,
-            author=author,
-            title='Recipe Title',
-            description='Recipe Description',
-            slug='recipe-slug',
-            preparation_time=10,
-            preparation_time_unit='Minutos',
-            servings=5,
-            servings_unit='Porções',
-            preparation_steps='Recipe Preparation Steps',
-            preparation_step_is_html=False,
-            is_published=True,
-        )
-
         response = self.client.get(reverse('recipes:home'))
         content = response.content.decode('utf-8')
         context_recipes = response.context['recipes']
